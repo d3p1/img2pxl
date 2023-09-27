0972375de4452e30df3922c699c277ed132b5346
Cristian Marcelo de Picciotto <d3p1@d3p1.dev>
2023-09-27 20:15:29 +0000

    feat: add image builder API [#1]

---
+++ src/app/core/api/image-builder.ts
@@ -0,0 +1,14 @@
+/**
+ * @description Image builder interface
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+import { IPixel } from "./data/particle/pixel";
+
+export default interface IImageBuilder {
+  /**
+   * Build image. Init pixels related to image
+   *
+   * @returns {Array<IPixel>}
+   */
+  build(): IPixel[];
+}
