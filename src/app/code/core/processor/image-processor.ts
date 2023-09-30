d1c541fd1fcaeab65620b728b03a78883029f1de
Cristian Marcelo de Picciotto <d3p1@d3p1.dev>
2023-09-30 00:13:41 +0000

    feat: improve image processor [#1]

--- src/app/code/core/processor/image-processor.ts
+++ src/app/code/core/processor/image-processor.ts
@@ -20,5 +20,41 @@ export default class ImageProcessor implements IImageProcessor {
   /**
    * @inheritdoc
    */
-  public render(): void {}
+  public render(): void {
+    for(const pixel of this._pixels) {
+      this._renderPixel(pixel)
+      this._updatePixel(pixel)
+    }
+  }
+
+  /**
+   * Render pixel
+   * 
+   * @param   {IPixel} pixel 
+   * @returns {void}
+   */
+  protected _renderPixel(pixel: IPixel): void {
+    this._context.fillStyle = this.#getFillStyleFromPixel(pixel)
+    this._context.rect(pixel.x, pixel.y, pixel.size, pixel.size)
+  }
+
+  /**
+   * Update pixel 
+   * 
+   * @param   {IPixel} pixel
+   * @returns {void} 
+   */
+  protected _updatePixel(pixel: IPixel): void {
+    pixel.update()
+  }
+
+  /**
+   * Get fill style from pixel
+   * 
+   * @param   {IPixel} pixel 
+   * @returns {string}
+   */
+  #getFillStyleFromPixel(pixel: IPixel): string {
+    return `rgba(${pixel.color.join(',')})`
+  }
 }
