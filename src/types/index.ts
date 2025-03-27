230d60b8769ee64f462918dbdca3cbc698fd927f
C. M. de Picciotto <d3p1@d3p1.dev>
2025-03-27 16:27:59 +0000

    refactor: improve type management [#4][#5]

---
+++ src/types/index.ts
@@ -0,0 +1,55 @@
+/**
+ * @description Types
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+
+/**
+ * @note Image that should be rendered
+ */
+export interface ImgSource {
+  src: string
+  width: number
+  height: number
+  resolution: {
+    width: number
+    height: number
+  }
+  pixel?: {
+    size?: number
+    motion?: {
+      displacement?: {
+        frequency?: number
+        amplitude?: number
+      }
+    }
+  }
+  motion?: {
+    noise?: {
+      src?: string
+      frequency?: number
+      amplitude?: number
+    }
+  }
+}
+
+/**
+ * @note Image by breakpoint
+ */
+export interface ImgSourceCollection {
+  [breakpoint: number]: ImgSource
+}
+
+/**
+ * @note Configuration that sets up library behavior
+ */
+export interface Config {
+  containerSelector?: string
+  images: ImgSourceCollection
+  pointer?: {
+    src?: string
+    size?: number
+    trailing?: {
+      factor?: number
+    }
+  }
+}
