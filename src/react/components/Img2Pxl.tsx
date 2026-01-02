12856f44fcea3e2dd1a8392c88419971cfbe4e9a
C. M. de Picciotto <d3p1@d3p1.dev>
2026-01-02 15:53:16 +0000

    fix: adjust react component initialization to use component HTML element [#9]

--- src/react/components/Img2Pxl.tsx
+++ src/react/components/Img2Pxl.tsx
@@ -2,10 +2,12 @@
  * @description Component
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
+'use client'
+
 import type {Img2PxlProps} from '../types'
 import useImg2Pxl from '../hooks/useImg2Pxl.js'
 
 export default function Img2Pxl(props: Img2PxlProps) {
-  useImg2Pxl(props)
+  useImg2Pxl({...props, containerSelector: '#img2pxl'})
   return <div id="img2pxl"></div>
 }
