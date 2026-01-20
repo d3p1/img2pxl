230aa59418dbb4112f0a5d99d7909522399d6f86
C. M. de Picciotto <d3p1@d3p1.dev>
2026-01-20 00:26:21 +0000

    docs: upgrade library in demo page [#1]

--- www/app/_component/Lib.tsx
+++ www/app/_component/Lib.tsx
@@ -5,66 +5,10 @@
 'use client'
 
 import {useImg2Pxl} from '@d3p1/img2pxl/react'
+import {lib} from '@/app/_etc/lib'
 
 export default function Lib() {
-  useImg2Pxl({
-    images: {
-      0: {
-        src: '/img2pxl/media/images/lib/meisje-met-de-parel.png',
-        width: 512 * 0.5,
-        height: 600 * 0.5,
-        resolution: {
-          width: 512 * 0.3,
-          height: 600 * 0.3,
-        },
-        pixel: {
-          size: 2,
-          alphaTest: 0.9,
-          motion: {
-            displacement: {
-              frequency: 1,
-              amplitude: 40,
-            },
-          },
-        },
-        motion: {
-          noise: {
-            frequency: 0.025,
-            amplitude: 40,
-          },
-        },
-      },
-      820: {
-        src: '/img2pxl/media/images/lib/meisje-met-de-parel.png',
-        width: 512 * 1.3,
-        height: 600 * 1.3,
-        resolution: {
-          width: 512 * 0.5,
-          height: 600 * 0.5,
-        },
-        pixel: {
-          size: 2,
-          motion: {
-            displacement: {
-              frequency: 1,
-              amplitude: 40,
-            },
-          },
-        },
-        motion: {
-          noise: {
-            frequency: 0,
-            amplitude: 0,
-          },
-        },
-      },
-    },
-    pointer: {
-      size: 0.1,
-    },
-    containerSelector: '#img2pxl',
-    isDebugging: true,
-  })
+  useImg2Pxl(lib)
 
   return (
     <div
---
+++ www/app/_etc/lib.ts
@@ -0,0 +1,62 @@
+/**
+ * @description Library configuration
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+export const lib = {
+  images: {
+    0: {
+      src: '/img2pxl/media/images/lib/meisje-met-de-parel.png',
+      width: 512 * 0.5,
+      height: 600 * 0.5,
+      resolution: {
+        width: 512 * 0.3,
+        height: 600 * 0.3,
+      },
+      pixel: {
+        size: 2,
+        alphaTest: 0.9,
+        motion: {
+          displacement: {
+            frequency: 1,
+            amplitude: 40,
+          },
+        },
+      },
+      motion: {
+        noise: {
+          frequency: 0.025,
+          amplitude: 40,
+        },
+      },
+    },
+    820: {
+      src: '/img2pxl/media/images/lib/meisje-met-de-parel.png',
+      width: 512 * 1.3,
+      height: 600 * 1.3,
+      resolution: {
+        width: 512 * 0.5,
+        height: 600 * 0.5,
+      },
+      pixel: {
+        size: 2,
+        motion: {
+          displacement: {
+            frequency: 1,
+            amplitude: 40,
+          },
+        },
+      },
+      motion: {
+        noise: {
+          frequency: 0,
+          amplitude: 0,
+        },
+      },
+    },
+  },
+  pointer: {
+    size: 0.1,
+  },
+  containerSelector: '#img2pxl',
+  isDebugging: true,
+}
